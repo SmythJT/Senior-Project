11ae4929d7a355d74ffd12fb8eb2afbb6d56192d
SmythJT <37964875+SmythJT@users.noreply.github.com>
2018-03-31 15:25:42 -0600

    Initial Commit

---
+++ Assets/DieOnDestroyer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieOnDestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.name == "Destroyer")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/ObstacleSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+    public GameObject obstacle;
+    public float TimeTillObjectSpawn = 3;
+    private float CurrentTimer;
+	// Use this for initialization
+	void Start () {
+		CurrentTimer = TimeTillObjectSpawn;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CurrentTimer -= Time.deltaTime;
+        if(CurrentTimer <= 0)
+        {
+            CurrentTimer = TimeTillObjectSpawn;
+            Instantiate(obstacle, transform);
+        }
+	}
+}
