14b53593d5c50994f85d84bbdf8cf452365257d2
SmythJT <37964875+SmythJT@users.noreply.github.com>
2018-06-04 18:56:14 -0600

    Finished Main Menu Button
    Says it all, Finished the button, added music, and removed a troublesome script that I was trying to get to work.

---
+++ Assets/ChangeScenes_Button.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes_Button : MonoBehaviour {
+
+
+    public void OnMouseUp()
+    {
+        SceneManager.LoadScene("Infinite");
+    }
+}
